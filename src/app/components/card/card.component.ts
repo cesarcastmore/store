602279277e11846695b632ddcca2d8367f03a668
Cesar Castillo <ccastillo@datafierce.com>
2022-04-13 20:29:54 -0500

    Input binding

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+  unit_price: number=32;
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/components/description/description.component.ts
+++ src/app/components/description/description.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-description',
@@ -7,8 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DescriptionComponent implements OnInit {
 
-  description: string='Many desktop';
-  price: number= 20;
+  @Input() description: string='Many desktop';
+  @Input() price: number= 20;
   claseProducto: string='product-name';
 
   
