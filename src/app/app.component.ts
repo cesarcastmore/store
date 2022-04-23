72d8ec8495a3380e15e56cdf3587c086a5c58d76
Cesar Castillo <ccastillo@datafierce.com>
2022-04-22 19:40:28 -0500

    directiva de estilo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,19 @@ import { menus } from './data/menus';
 import { Menu } from './models/menu';
 import {ToastService} from './services/toast.service';
 
+//Estoy importanta la contante que tendra las configraciones que cambiaran mi estilo de la aplicacion 
+import { configuration } from './data/configuration';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+
+  //Estoy iniciando esta variable para utilizar en la platilla html
+  configuration= configuration;
 
   list_menus: Menu[] = menus;
 
