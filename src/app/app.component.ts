0cd04c5ff6b9e8246d68c3c5cd14e3b73e18e885
Cesar Castillo <ccastillo@datafierce.com>
2022-04-25 20:00:41 -0500

    introducciona observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { menus } from './data/menus';
 import { Menu } from './models/menu';
 import {ToastService} from './services/toast.service';
+
+import {Observable, Observer} from 'rxjs';
 
 //Estoy importanta la contante que tendra las configraciones que cambiaran mi estilo de la aplicacion 
 import { configuration } from './data/configuration';
@@ -11,7 +13,7 @@ import { configuration } from './data/configuration';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
 
   //Estoy iniciando esta variable para utilizar en la platilla html
@@ -23,6 +25,61 @@ export class AppComponent {
 
   title = 'store';
   constructor(public _toastService: ToastService){
+
+  }
+
+  ngOnInit(): void {
+
+
+    let cambios: Observable<number>= this.createObservable();
+
+    console.log("INICIO");
+    cambios.subscribe(value=> console.log(value));
+    console.log("FIN");
+
+
+
+
+
+  }
+
+
+  public createObservable(): Observable<number>{
+
+    return Observable.create((observer: Observer<number>)=> {
+
+      let contador: number=0
+
+      setTimeout(()=>{
+        contador +=1;
+        observer.next(contador)
+
+      }, 1000);
+
+
+      setTimeout(()=>{
+        contador +=1;
+        observer.next(contador)
+
+      }, 2000);
+
+
+      setTimeout(()=>{
+        contador +=1;
+        observer.next(contador)
+
+      }, 4000);
+
+
+      setTimeout(()=>{
+        observer.complete()
+
+        console.log("termine");
+
+      }, 5000);
+
+    })
+
 
   }
 
